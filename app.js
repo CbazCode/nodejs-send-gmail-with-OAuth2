6deb4bb6095d25b3aac902347b1e260c35e42e28
Sebastian Asis <sasisromero10@gmail.com>
2021-02-22 01:57:23 -0500

    :dart: IMPROVE: Pusheando

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const { google } = require('googleapis');
 
 
 // These id's and secrets should come from .env file.
-const CLIENT_ID = '1008409694345-n0tap2lmt0unfs9mfb4cqg1klo4f3rkt.apps.googleusercontent.com';
-const CLEINT_SECRET = 'U1miyqVRsZ0qGuybk3b0l5ad';
+const CLIENT_ID = 'YOUR CLIENT ID';
+const CLEINT_SECRET = 'YOUR CLIENT SECRET';
 const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
-const REFRESH_TOKEN = '1//04PuYsdAtNCWDCgYIARAAGAQSNwF-L9IrY9DkzIkk92IuqGrX1W58Lx8wTwZv_pOPuFW9nqcRzGH7SgaszX96SBd7TG2wmvVRsI8';
+const REFRESH_TOKEN = 'YOUR REFRESH TOKEN';
 
 const oAuth2Client = new google.auth.OAuth2(
   CLIENT_ID,
@@ -34,13 +34,13 @@ async function sendMail() {
     let HTMLcontent = `
       <h1>Prueba de estilos</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab commodi, expedita nesciunt dolore quia nulla vero saepe est. Ipsam, odio.</p>
-      <button style="color:blue;text-align:center;" ><a style="text-decoration:none;" href = "https://www.youtube.com/channel/UCs1DHnQij0wp0lP1V96rukg">Hello from gmail email using API</a></button>
+      <button style="color:blue;text-align:center;" ><a style="text-decoration:none;" href = "https://www.youtube.com/channel/UCs1DHnQij0wp0lP1V96rukg">Click here!</a></button>
     `
 
     const mailOptions = {
-      from: 'SEBASTIAN1010 <alexandercode1464@gmail.com>',
-      to: 'sasisromero10@gmail.com',
-      subject: 'Hello from gmail using API 4',
+      from: 'SENDED AUTHORIZADED',
+      to: 'EMAIL',
+      subject: 'Hello from gmail using API',
       text: 'Hello from gmail email using API',
       html: HTMLcontent,
     };
